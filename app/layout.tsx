4a2f34673cd3d55c833252e8a84bf6ce24e0a194
Ankit Kumar <ankit_kumar6@persistent.com>
2024-05-30 12:18:10 +0530

    completed required features

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+              attribute="class"
+              defaultTheme="system">
+            {children}
+            <Toaster/>
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
